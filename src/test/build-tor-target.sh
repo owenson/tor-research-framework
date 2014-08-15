e5244c79a3b72a38817409d2394e1e47f3a589a3
twilsonb <twilsonb mac com>
2014-08-16 02:42:18 +1000

    Minor updates to design, directory URLs, and test scripts

--- src/test/build-tor-target.sh
+++ src/test/build-tor-target.sh
@@ -7,13 +7,34 @@
 #   harden: disable features, enable extra security
 # This assists in determining vulnerable features during testing
 
-# Run this script in the directory containing the tor source distribution
+# Run this script in a directory containing the tor source distribution
 
 PREFIX_BASE=$HOME/tor
-EXTRA_LIBDIR=$HOME/tor/libs
+EXT_DIR=$HOME/tor/external
+EXT_INCDIR=$HOME/tor/external/include
+EXT_LIBDIR=$HOME/tor/external/lib
 
-CC_OPTS="--with-libevent-dir=$EXTRA_LIBDIR --disable-asciidoc --disable-dependency-tracking"
-MAKE="make"
+ 
+# Required header and library for dmalloc
+# We could also -DFINI_DMALLOC=0, but instead we turn off -Werror
+CPPFLAGS="-I$EXT_INCDIR"
+LDFLAGS="-L$EXT_LIBDIR"
+# For the threads tests to run, the number below should be set between 5 and 30
+# This asks dmalloc to wait for the thread manager to start up
+# These variables were generated using "dmalloc -o <number>", 
+# then modified to correctly parse in bash
+# On my system, lockon 3 passes, but 2 fails then hangs, and 0 goes recursive
+DMALLOC_OPTIONS="lockon=3"
+export DMALLOC_OPTIONS
+# Set the OS X library path to include the dmalloc library
+export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:$EXT_LIBDIR
+
+# On my system, libevent and (the working version of) openssl 
+# are in a non-system directory
+# We also use dmalloc across all builds to catch memory errors early
+# We disable documentation and dependency tracking to speed up the builds
+CC_OPTS="--with-libevent-dir=$EXT_LIBDIR --with-openssl-dir=$EXT_LIBDIR --with-dmalloc CPPFLAGS=$CPPFLAGS LDFLAGS=$LDFLAGS --disable-asciidoc --disable-dependency-tracking"
+MAKE="make -j4"
 
 date
 
@@ -22,7 +43,7 @@ date
 # Enable extras, disable security, use (older) system libraries
 # We could also use libnatpmp if available: --enable-nat-pmp
 $MAKE clean && \
-./configure --prefix=$PREFIX_BASE/tor-features-install $CC_OPTS --enable-gcc-warnings-advisory --enable-instrument-downloads --enable-upnp --with-libminiupnpc-dir=$EXTRA_LIBDIR --disable-gcc-hardening --disable-linker-hardening && $MAKE && $MAKE test && $MAKE install && date && \
+./configure --prefix=$PREFIX_BASE/tor-features-install $CC_OPTS --enable-gcc-warnings-advisory --enable-instrument-downloads --enable-upnp --with-libminiupnpc-dir=$EXT_LIBDIR --disable-gcc-hardening --disable-linker-hardening && $MAKE && $MAKE test && $MAKE install && date && \
 \
 # Use defaults wherever possible, including (older) system libraries
 $MAKE clean && \
@@ -30,7 +51,7 @@ $MAKE clean && \
 \
 # Disable extras, options, enable security, use (newer) libraries
 $MAKE clean && \
-./configure --prefix=$PREFIX_BASE/tor-harden-install $CC_OPTS --enable-gcc-warnings --disable-curve25519 --disable-transparent --disable-largefile --enable-expensive-hardening --with-openssl-dir=$EXTRA_LIBDIR --with-zlib-dir=$EXTRA_LIBDIR && $MAKE && $MAKE test && $MAKE install && date && \
+./configure --prefix=$PREFIX_BASE/tor-harden-install $CC_OPTS --enable-gcc-warnings-advisory --disable-curve25519 --disable-transparent --disable-largefile --enable-expensive-hardening --with-openssl-dir=$EXT_LIBDIR --with-zlib-dir=$EXT_LIBDIR && $MAKE && $MAKE test && $MAKE install && date && \
 
 $MAKE clean
 
--- src/test/run-tor-features.sh
+++ src/test/run-tor-features.sh
@@ -6,6 +6,19 @@ CONTROL=/test/tor/vidalia/build/src/vidalia/Vidalia.app/Contents/MacOS/Vidalia
 LOG_LEVEL=info
 DATA_DIR=/test/tor/tor-features-data
 
+# For dmalloc to avoid threading conflicts, 
+# the number below should be set between 5 and 30
+# This asks dmalloc to wait for the thread manager to start up
+# These variables were generated using "dmalloc -o <number>", 
+# then modified to correctly parse in bash
+# On my system, lockon 3 passes, but 2 fails then hangs, and 0 goes recursive
+DMALLOC_OPTIONS="lockon=3"
+export DMALLOC_OPTIONS
+# Set the OS X library path to include the dmalloc library
+EXT_LIBDIR=$HOME/tor/external/lib
+export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:$EXT_LIBDIR
+
+
 date
 $CONTROL --datadir $DATA_DIR --logfile $DATA_DIR/vidalia.$LOG_LEVEL.log --loglevel $LOG_LEVEL &
 $TOR -f $DATA_DIR/torrc.cache_only --DataDirectory $DATA_DIR
