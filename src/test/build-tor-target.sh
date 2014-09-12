85910c84738da4bfeeeaf9d5deb9c4f50b3346f2
twilsonb <twilsonb mac com>
2014-09-13 07:34:15 +1000

    Update sample configurations for fuzzing targets

--- src/test/build-tor-target.sh
+++ src/test/build-tor-target.sh
@@ -14,26 +14,51 @@ EXT_DIR=$HOME/tor/external
 EXT_INCDIR=$HOME/tor/external/include
 EXT_LIBDIR=$HOME/tor/external/lib
 
- 
-# Required header and library for dmalloc
-# We could also -DFINI_DMALLOC=0, but instead we turn off -Werror
+O_FAST="-Ofast -ffp-contract=fast -fslp-vectorize-aggressive -fstrict-enums"
+O_SIZE="-Oz"
+O_MIN="-O1"
+O_NONE="-O0"
+
+# to aid debugging
+#O_FAST=$O_NONE
+#O_SIZE=$O_NONE
+#O_MIN=$O_NONE
+
+# dmalloc doesn't seem to work on the latest builds without -DFINI_DMALLOC=1,
+# which is difficult to configure.
+# We might use the system guard malloc instead.
+
 CPPFLAGS="-I$EXT_INCDIR"
 LDFLAGS="-L$EXT_LIBDIR"
+# Required header and library for dmalloc
+# We could also -DFINI_DMALLOC=0, but instead we turn off -Werror
 # For the threads tests to run, the number below should be set between 5 and 30
 # This asks dmalloc to wait for the thread manager to start up
 # These variables were generated using "dmalloc -o <number>", 
 # then modified to correctly parse in bash
 # On my system, lockon 3 passes, but 2 fails then hangs, and 0 goes recursive
-DMALLOC_OPTIONS="lockon=3"
-export DMALLOC_OPTIONS
+#DMALLOC_OPTIONS="lockon=3"
+#export DMALLOC_OPTIONS
 # Set the OS X library path to include the dmalloc library
-export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:$EXT_LIBDIR
+#export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:$EXT_LIBDIR
 
 # On my system, libevent and (the working version of) openssl 
 # are in a non-system directory
 # We also use dmalloc across all builds to catch memory errors early
 # We disable documentation and dependency tracking to speed up the builds
-CC_OPTS="--with-libevent-dir=$EXT_LIBDIR --with-openssl-dir=$EXT_LIBDIR --with-dmalloc CPPFLAGS=$CPPFLAGS LDFLAGS=$LDFLAGS --disable-asciidoc --disable-dependency-tracking"
+
+#--with-dmalloc
+
+# we can also use --analyze for the clang static analyser
+# -ftrapv causes the sscanf test to crash / fail
+# as do -fsanitize=undefined-trap -fsanitize-undefined-trap-on-error
+# -fbounds-checking causes lots of incorrect deprecation warnings
+# -fsanitize-memory-track-origins increases memory usage, and causes warnings
+# PARANOIA activates extra asserts
+CLANG="clang -g -Wall -Wextra -DPARANOIA -fstack-protector -fsanitize=undefined-trap -fsanitize-undefined-trap-on-error -ftrapv"
+CC="$CLANG"
+
+CC_OPTS="--with-libevent-dir=$EXT_LIBDIR --with-openssl-dir=$EXT_LIBDIR CPPFLAGS=$CPPFLAGS --disable-asciidoc --disable-dependency-tracking --enable-gcc-warnings-advisory"
 MAKE="make -j4"
 
 date
@@ -42,16 +67,36 @@ date
 
 # Enable extras, disable security, use (older) system libraries
 # We could also use libnatpmp if available: --enable-nat-pmp
-$MAKE clean && \
-./configure --prefix=$PREFIX_BASE/tor-features-install $CC_OPTS --enable-gcc-warnings-advisory --enable-instrument-downloads --enable-upnp --with-libminiupnpc-dir=$EXT_LIBDIR --disable-gcc-hardening --disable-linker-hardening && $MAKE && $MAKE test && $MAKE install && date && \
-\
+CC="$CLANG $O_FAST -flto" \
+LDFLAGS_FLTO="$LDFLAGS -flto" \
+$MAKE clean \
+&& \
+./configure --prefix=$PREFIX_BASE/tor-features-install CC="$CC" $CC_OPTS --enable-instrument-downloads --enable-upnp --with-libminiupnpc-dir=$EXT_LIBDIR --disable-gcc-hardening --disable-linker-hardening LDFLAGS="$LDFLAGS_FLTO" \
+&& $MAKE \
+&& $MAKE test \
+&& $MAKE install \
+&& date || exit
+
 # Use defaults wherever possible, including (older) system libraries
-$MAKE clean && \
-./configure --prefix=$PREFIX_BASE/tor-default-install $CC_OPTS --enable-gcc-warnings-advisory && $MAKE && $MAKE test && $MAKE install && date && \
-\
-# Disable extras, options, enable security, use (newer) libraries
-$MAKE clean && \
-./configure --prefix=$PREFIX_BASE/tor-harden-install $CC_OPTS --enable-gcc-warnings-advisory --disable-curve25519 --disable-transparent --disable-largefile --enable-expensive-hardening --with-openssl-dir=$EXT_LIBDIR --with-zlib-dir=$EXT_LIBDIR && $MAKE && $MAKE test && $MAKE install && date && \
+CC="$CLANG $O_SIZE" \
+$MAKE clean \
+&& \
+./configure --prefix=$PREFIX_BASE/tor-default-install CC="$CC" $CC_OPTS LDFLAGS="$LDFLAGS" \
+&& $MAKE \
+&& $MAKE test \
+&& $MAKE install \
+&& date || exit
+
+# Disable extras, options, enable security, use (newer) libraries, 
+# avoid optimising too much
+CC="$CLANG $O_MIN -fstrict-aliasing -Wstrict-aliasing" \
+$MAKE clean \
+&& \
+./configure --prefix=$PREFIX_BASE/tor-harden-install CC="$CC" $CC_OPTS --disable-curve25519 --disable-transparent --disable-largefile --enable-expensive-hardening --with-openssl-dir=$EXT_LIBDIR --with-zlib-dir=$EXT_LIBDIR LDFLAGS="$LDFLAGS" \
+&& $MAKE \
+&& $MAKE test \
+&& $MAKE install \
+&& date || exit
 
 $MAKE clean
 
--- src/test/run-tor-features.sh
+++ src/test/run-tor-features.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Run the tor "features" target in cache-only mode
-TOR=/test/tor/tor-features-install/bin/tor
+#TOR=/test/tor/tor-features-install/bin/tor
+TOR=/Users/twilsonb/Library/Developer/Xcode/DerivedData/tor-dwffosaqftyouufwjtqkiwgynlvd/Build/Products/Debug/tor
 CONTROL=/test/tor/vidalia/build/src/vidalia/Vidalia.app/Contents/MacOS/Vidalia
 LOG_LEVEL=info
 DATA_DIR=/test/tor/tor-features-data
@@ -12,11 +13,11 @@ DATA_DIR=/test/tor/tor-features-data
 # These variables were generated using "dmalloc -o <number>", 
 # then modified to correctly parse in bash
 # On my system, lockon 3 passes, but 2 fails then hangs, and 0 goes recursive
-DMALLOC_OPTIONS="lockon=3"
-export DMALLOC_OPTIONS
+#DMALLOC_OPTIONS="lockon=3"
+#export DMALLOC_OPTIONS
 # Set the OS X library path to include the dmalloc library
-EXT_LIBDIR=$HOME/tor/external/lib
-export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:$EXT_LIBDIR
+#EXT_LIBDIR=$HOME/tor/external/lib
+#export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:$EXT_LIBDIR
 
 
 date
